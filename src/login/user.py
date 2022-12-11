da96bbf557ccb2229641b8778520c72b18c8c496
Pope <j.rubio.piornal@gmail.com>
2022-12-11 14:11:24 +0100

    add user tests

--- src/login/user.py
+++ src/login/user.py
@@ -12,41 +12,54 @@ class User(BaseModel):
     email: str = ''
     password: str = ''
 
-    def __init__(self, **data):
-        self.set_username(
-            data['username'] if 'username' in data.keys() else ''
-        )
-        self.set_email(
-            data['email'] if 'email' in data.keys() else ''
-        )
-        self.set_password(
-            data['password'] if 'password' in data.keys() else ''
-        )
+    @validator('id')
+    def set_id(cls, id: int) -> int:
+        if type(id) != int:
+            return int(id)
+        return id                        
         
     @validator('username')
     def set_username(cls, username: str) -> str:
-        if '' in username:
-            raise ValidationError('username cannot be empty')
-        if re.match('[0-9]+', username):
-            raise ValidationError('username cannot contains numbers')
+        if username == '':
+            raise ValueError('username cannot be empty')
+        if re.findall(r'[\s]+', username):
+            raise ValueError('username cannot have spaces')
+        if re.findall(r'[0-9]+', username):
+            raise ValueError('username cannot contains numbers')
         return username
 
     @validator('email')
     def set_email(cls, email: str) -> str:
-        if not re.fullmatch('[a-zA-Z0-9\.\_\-\+]+@[a-zA-Z0-9\.\_\-\+]+\.[a-z]\{1,4\}', email):
-            raise ValidationError('email wrong spelling')
+        if email == '':
+            raise ValueError('email cannot be empty')
+        if not re.fullmatch(r'[a-zA-Z0-9\.\_\-\+]+@[a-zA-Z0-9\.\_\-\+]+\.[a-z]{1,4}', email):
+            raise ValueError('email wrong spelling')
         return email
     
     @validator('password')
     def set_password(cls, password: str) -> str:
-        if password.__len__ == 256:
+        if password.__len__() == 64:
             return password
-        if not 6 < password.__le__ < 10:
-            raise ValidationError('password has not the correct length')
+        if password == '':
+            raise ValueError('password cannot be empty')
+        if not 6 < password.__len__() < 10:
+            raise ValueError(f'password has not the correct length: {password.__len__()}')
         if not (
-                re.match('[a-z]+', password) 
-                and re.match('[A-Z]+', password) 
-                and re.match('[0-9]+', password)
+                re.findall(r'[a-z]+', password)
+                and re.findall(r'[A-Z]+', password) 
+                and re.findall(r'[0-9]+', password)
             ):
-            raise ValidationError('password does not match the correct form')
-        return sha256(b'{pass}'.format(pass=password)).hexdigest()
+            raise ValueError('password does not match the correct form')
+        return sha256(f'{password}'.encode('utf-8')).hexdigest()
+
+    def get_id(self) -> int:
+        return self.id
+
+    def get_username(self) -> str:
+        return self.username
+
+    def get_email(self) -> str:
+        return self.email
+    
+    def get_password(self) -> str:
+        return self.password
---
+++ tests/login/test_user.py
@@ -0,0 +1,174 @@
+from hashlib import sha256
+from pytest import raises
+from src.login.user import User
+
+
+def encrypt(string: str) -> str:
+    return sha256(string.encode('utf-8')).hexdigest()
+
+
+class TestUser:
+    def test_create_user_ok(self) -> None:
+        """
+        Test creating new user successfully
+        """
+        id: int = 0
+        username: str = 'pope_test'
+        email: str = 'pope@chatapp.com'
+        password: str = '1Qaz2Wsx'
+
+        user = User(
+            id=id,
+            username=username,
+            email=email,
+            password=password
+        )
+        assert id == user.get_id(), \
+            f'Error comparing id: expected {id}, got {user.get_id}'
+        assert username == user.get_username(), \
+            f'Error comparing username: expected {username}, got {user.get_username}'
+        assert email == user.get_email(), \
+            f'Error comparing email: expected {email}, got {user.get_email}'
+        assert encrypt(password) == user.get_password(), \
+            f'Error comparing password: expected {password}, got {user.password}'
+
+    def test_set_user_ok(self) -> None:
+        """
+        Test create object from existing user successfully
+        """
+        id: int = 0
+        username: str = 'pope_test'
+        email: str = 'pope@chatapp.com'
+        password: str = encrypt('1Qaz2Wsx')
+
+        user = User(
+            id=id,
+            username=username,
+            email=email,
+            password=password
+        )
+        assert id == user.get_id(), \
+            f'Error comparing id: expected {id}, got {user.get_id}'
+        assert username == user.get_username(), \
+            f'Error comparing username: expected {username}, got {user.get_username}'
+        assert email == user.get_email(), \
+            f'Error comparing email: expected {email}, got {user.get_email}'
+        assert password == user.get_password(), \
+            f'Error comparing password: expected {password}, got {user.password}'
+
+    def test_username_empty(self) -> None:
+        """
+        Test empty username
+        """
+        username = ''
+        with raises(ValueError) as excinfo:
+            user = User(username=username)
+        assert 'username cannot be empty' in str(excinfo.value)
+
+    def test_username_with_spaces(self) -> None:
+        """
+        Test username with spaces
+        """
+        username = 'user name'
+        with raises(ValueError) as excinfo:
+            user = User(username=username)
+        assert 'username cannot have spaces' in str(excinfo.value)
+        
+    def test_username_with_number(self) -> None:
+        """
+        Test username with numbers
+        """
+        username = 'user1name2'
+        with raises(ValueError) as excinfo:
+            user = User(username=username)
+        assert 'username cannot contains numbers' in str(excinfo.value)
+
+    def test_email_empty(self) -> None:
+        """
+        Test empty email
+        """
+        email = ''
+        with raises(ValueError) as excinfo:
+            user = User(email=email)
+        assert 'email cannot be empty' in str(excinfo.value)
+    
+    def test_email_missing_symbols_1(self) -> None:
+        """
+        Test email missing @
+        """
+        email = 'popechatapp.com'
+        with raises(ValueError) as excinfo:
+            user = User(email=email)
+        assert 'email wrong spelling' in str(excinfo.value)
+
+    def test_email_missing_symbols_2(self) -> None:
+        """
+        Test email missing . extension
+        """
+        email = 'pope@chatappcom'
+        with raises(ValueError) as excinfo:            
+            user = User(email=email)                 
+        assert 'email wrong spelling' in str(excinfo.value)
+
+    def test_password_empty(self) -> None:
+        """
+        Test empty password
+        """
+        password = ''        
+        with raises(ValueError) as excinfo:
+            user = User(password=password)
+        assert 'password cannot be empty' in str(excinfo.value)
+    
+    def test_password_wrong_len_1(self) -> None:
+        """
+        Test password length lower than expected
+        """
+        password = '1Qaz2'        
+        with raises(ValueError) as excinfo:
+            user = User(password=password)
+        assert f'password has not the correct length: {password.__len__()}' in str(excinfo.value)
+
+    def test_password_wrong_len_2(self) -> None:
+        """
+        Test password length higher than expected
+        """
+        password = '1Qaz2Wsx3Ed'        
+        with raises(ValueError) as excinfo:
+            user = User(password=password)
+        assert f'password has not the correct length: {password.__len__()}' in str(excinfo.value)
+
+    def test_password_wrong_spelling_1(self) -> None:
+        """
+        Test password wrong spelling without lower case letters
+        """
+        password = '1QAZ2WSX'        
+        with raises(ValueError) as excinfo:
+            user = User(password=password)
+        assert 'password does not match the correct form' in str(excinfo.value)
+    
+    def test_password_wrong_spelling_2(self) -> None:
+        """
+        Test password wrong spelling without upper case letters
+        """
+        password = '1qaz2wsx'        
+        with raises(ValueError) as excinfo:
+            user = User(password=password)
+        assert 'password does not match the correct form' in str(excinfo.value)
+
+    def test_password_wrong_spelling_3(self) -> None:
+        """
+        Test password wrong spelling without numbers
+        """
+        password = 'QazWsxEdc'        
+        with raises(ValueError) as excinfo:
+            user = User(password=password)
+        assert 'password does not match the correct form' in str(excinfo.value)
+    
+    def test_password_wrong_hashed(self) -> None:
+        """
+        Test password wrong spelling without lower case letters
+        """
+        password = encrypt('1Qaz2Wsx3E')[:62]
+        with raises(ValueError) as excinfo:
+            user = User(password=password)
+        assert f'password has not the correct length: {password.__len__()}' in str(excinfo.value)
